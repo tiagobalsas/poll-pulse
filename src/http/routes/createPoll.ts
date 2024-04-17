efaa94f2937ebb524b9fd6fe9d47227f6b7a11ed
Tiago Santos <tiago881@gmail.com>
2024-04-17 15:46:23 -0300

    feat(route)add: pollOption

--- src/http/routes/createPoll.ts
+++ src/http/routes/createPoll.ts
@@ -7,12 +7,22 @@ export async function CreatePoll(app: FastifyInstance) {
   app.post("/polls", async (request, reply) => {
     const createPollBody = z.object({
       title: z.string(),
+      options: z.array(z.string()),
     });
 
-    const { title } = createPollBody.parse(request.body);
+    const { title, options } = createPollBody.parse(request.body);
 
     const poll = await prisma.poll.create({
-      data: { title },
+      data: {
+        title,
+        options: {
+          createMany: {
+            data: options.map((option) => {
+              return { title: option };
+            }),
+          },
+        },
+      },
     });
 
     return reply.status(201).send({ pollId: poll.id });
