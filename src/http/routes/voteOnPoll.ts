e55206b80e9867e4113ac5c36a178ed6b3b1fe80
“Tiago <tiago881@gmail.com>
2024-04-18 10:31:21 -0300

    WIP:add poll voting route

---
+++ src/http/routes/voteOnPoll.ts
@@ -0,0 +1,22 @@
+import { randomUUID } from "crypto";
+import fastify, { FastifyInstance } from "fastify";
+import { z } from "zod";
+
+export async function voteOnPoll(app: FastifyInstance) {
+  app.post("/polls/:pollId/votes", async (request, reply) => {
+    const voteOnPollBody = z.object({
+      pollOptionId: z.string().uuid(),
+    });
+
+    const voteOnPollParams = z.object({
+      pollId: z.string().uuid(),
+    });
+
+    const { pollId } = voteOnPollParams.parse(request.params);
+    const { pollOptionId } = voteOnPollBody.parse(request.body);
+
+    const sessionId = randomUUID();
+
+    return reply.status(201).send();
+  });
+}
