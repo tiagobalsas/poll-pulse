0a2e3cd1e1af52573834b931e54a18b14c6b035d
Tiago Santos <tiago881@gmail.com>
2024-04-18 10:31:21 -0300

    WIP:add poll voting route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,19 @@
 import fastify from "fastify";
 import { createPoll } from "./routes/createPoll";
-import { getPoll } from "./routes/get-poll";
+import { getPoll } from "./routes/getPoll";
+import { voteOnPoll } from "./routes/voteOnPoll";
+import cookie from "@fastify/cookie";
 
 const app = fastify();
 
+app.register(cookie, {
+  secret: "4ab5cc06f65f87d70e7ba31a6ae0a906",
+  hook: "onRequest",
+});
+
 app.register(createPoll);
 app.register(getPoll);
+app.register(voteOnPoll);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP Server is running!");
