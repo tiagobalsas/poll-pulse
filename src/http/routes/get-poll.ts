9af176cfbbed3e3f70249186f14fe3be4e1b5375
“Tiago <tiago881@gmail.com>
2024-04-18 09:08:37 -0300

    fix(uuid()):changes cuid to uuid

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -6,23 +6,14 @@ import { prisma } from "../../lib/prisma";
 export async function getPoll(app: FastifyInstance) {
   app.get("/polls/:pollId", async (request, reply) => {
     const getPollParams = z.object({
-      pollId: z.string().cuid(),
+      pollId: z.string().uuid(),
     });
 
     const { pollId } = getPollParams.parse(request.params);
 
     const poll = await prisma.poll.findUnique({
-      where: {
-        id: pollId,
-      },
-      include: {
-        options: {
-          select: {
-            id: true,
-            title: true,
-          },
-        },
-      },
+      where: { id: pollId },
+      include: { options: { select: { id: true, title: true } } },
     });
 
     return reply.send({ poll });
