ef9ee8d011d30548253ce2aafc2626c3f183d7ef
Tiago Santos <tiago881@gmail.com>
2024-04-18 16:25:07 -0300

    WIP: Validade user vote

--- src/http/routes/voteOnPoll.ts
+++ src/http/routes/voteOnPoll.ts
@@ -1,6 +1,7 @@
 import { randomUUID } from "crypto";
-import fastify, { FastifyInstance } from "fastify";
+import  { FastifyInstance } from "fastify";
 import { z } from "zod";
+import { prisma } from "../../lib/prisma";
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post("/polls/:pollId/votes", async (request, reply) => {
@@ -28,6 +29,14 @@ export async function voteOnPoll(app: FastifyInstance) {
       });
     }
 
-    return reply.status(201).send({ sessionId });
+    await prisma.vote.create({
+      data: {
+        sessionId,
+        pollId,
+        pollOptionId,
+      },
+    });
+
+    return reply.status(201).send();
   });
 }
