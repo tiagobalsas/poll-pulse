676d468b052143a64653be5816be366386bb078d
Tiago Santos <tiago881@gmail.com>
2024-04-17 21:15:05 -0300

    feat(route)add: get poll id

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,11 @@
 import fastify from "fastify";
-import { CreatePoll } from "./routes/createPoll";
+import { createPoll } from "./routes/createPoll";
+import { getPoll } from "./routes/get-poll";
 
 const app = fastify();
 
-app.register(CreatePoll);
+app.register(createPoll);
+app.register(getPoll);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP Server is running!");
