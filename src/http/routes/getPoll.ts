426a12c5d22ec54646bf66bc32f722350b6884e1
Tiago Santos <tiago881@gmail.com>
2024-04-19 14:49:45 -0300

    feature(redis):Add functionality to retrieve and process poll results from Redis

--- src/http/routes/getPoll.ts
+++ src/http/routes/getPoll.ts
@@ -1,7 +1,8 @@
 import { FastifyInstance } from "fastify";
-import { z } from "zod";
+import { record, z } from "zod";
 
 import { prisma } from "../../lib/prisma";
+import { redis } from "../../lib/redis";
 
 export async function getPoll(app: FastifyInstance) {
   app.get("/polls/:pollId", async (request, reply) => {
@@ -15,6 +16,22 @@ export async function getPoll(app: FastifyInstance) {
       where: { id: pollId },
       include: { options: { select: { id: true, title: true } } },
     });
+
+    if (!poll) {
+      return reply.status(400).send({ message: "Poll not found." });
+    }
+
+    const result = await redis.zrange(pollId, 0, -1, "WITHSCORES");
+
+    const votes = result.reduce((obj, line, index) => {
+      if (index % 2 === 0) {
+        const score = result[index + 1];
+
+        Object.assign(obj, { [line]: Number(score) });
+      }
+
+      return obj;
+    }, {} as Record<string, number>);
 
     return reply.send({ poll });
   });
