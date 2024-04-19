f5137ab80f25d0663039932148b052141678ab29
Tiago Santos <tiago881@gmail.com>
2024-04-19 09:48:26 -0300

    feture(redis)add: increment vote in poll optionPoll

--- src/http/routes/voteOnPoll.ts
+++ src/http/routes/voteOnPoll.ts
@@ -2,6 +2,7 @@ import { randomUUID } from "crypto";
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { prisma } from "../../lib/prisma";
+import { redis } from "../../lib/redis";
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post("/polls/:pollId/votes", async (request, reply) => {
@@ -60,6 +61,8 @@ export async function voteOnPoll(app: FastifyInstance) {
         pollOptionId,
       },
     });
+
+    await redis.zincrby(pollId, 1, pollOptionId);
 
     return reply.status(201).send();
   });
