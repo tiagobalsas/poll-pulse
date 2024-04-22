6207f560a01dc94b8500bf6ef53aacb3a9233d2f
Tiago Santos <tiago881@gmail.com>
2024-04-22 10:33:10 -0300

    feat: Add WebSocket endpoint for live poll results

--- src/http/ws/poll-results.ts
+++ src/http/ws/poll-results.ts
@@ -1,12 +1,20 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { voting } from "../../utils/voting-pub-sub";
 
 export async function pollResults(app: FastifyInstance) {
   app.get(
     "/polls/:pollId/results",
     { websocket: true },
     (connection, request) => {
-      connection.socket.on("message", (message: string) => {
-        // falta criar a lógica
+      const getPollParams = z.object({
+        pollId: z.string().uuid(),
+      });
+
+      const { pollId } = getPollParams.parse(request.params);
+
+      voting.subscribe(pollId, (message) => {
+        connection.socket.send(JSON.stringify(message));
       });
     }
   );
